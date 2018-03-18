c79ef6729b933fc29c39b9a43508d0380d070eb2
Ougui9 <33538250+Ougui9@users.noreply.github.com>
2018-03-18 01:40:16 -0400

    init commit

---
+++ capsNet.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import numpy as np
+
+
+
+class Conv(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+
+
+
+
+
+
+
+
+if __name__=='__main__':
